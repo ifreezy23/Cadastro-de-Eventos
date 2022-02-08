6b3ecdd28f83ec364d83336dfa9133cb0da1aa43
ifreezy23 <joaomarcos_geris@hotmail.com>
2022-02-08 20:35:05 -0300

    Cadastro de Eventos

---
+++ Cadastro de Eventos.js
@@ -0,0 +1,31 @@
+
+let idade, data, palestrate, participante
+
+    console.log("Digite a sua idade.")
+
+    if(idade<18){
+        console.log("Cadastro permitido apenas para maiores de 18 anos.")
+    }else{
+        console.log("informe a data do evento.")
+        
+        if(data<20){
+            console.log("sentimos muito a data já inspirou")
+        }else{
+            console.log("Se você é um(a) palestrante digite 1")
+            
+            if(palestrante = 1){
+                console.log("Digite suas informações de cadastro!")
+            }else{
+                console.log("Então você é um participante!")
+                if(palestrante<100){
+                    console.log("Digite suas informações de cadastro.")
+                }else{
+                    console.log("infelizmente não possui mais vagas.")
+                }
+            }
+                
+            
+        }
+        
+    }
+    console.log("Operação Finalizada")
